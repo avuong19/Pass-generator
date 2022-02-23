60393863b44026e037eb0abedfedb2f6c0d25a26
Anh Vuong <vta19797@gmail.com>
2022-02-22 23:56:17 -0600

    confirm password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -30,12 +30,49 @@ console.log (getRandomSpecial());
 
 //Prompt to customize password
 var generatePassword = function(){
+  
+  var finalPassword;
+
   var passLength = window.prompt ("How long would your password be? Please pick a number between 8-128");
   passLength = parseInt (passLength);
 
   //Check number selected to see if within valid range
-  if
-};
+  if(passLength>128 || passLength<8){
+    window.alert("Please pick a number between 8-128");
+    generatePassword();
+  }
+  //Confirm password criteria
+  else{
+    var confirmLower = confirm ("Do you want lower case in your password?");
+    var confirmUpper = confirm ("Do you want upper case in your password?");
+    var confirmNumber = confirm ("Do you want number in your password?");
+    var confirmSpecial = confirm ("Do you want special character in your password?");
+      if(confirmLower|| confirmUpper|| confirmNumber|| confirmSpecial ){
+        if (confirmLower){
+          finalPassword= finalPassword+ getRandomLower;
+        }
+        if (confirmUpper){
+          finalPassword=finalPassword+ getRandomUpper;
+        }
+        if (confirmNumber){
+          finalPassword=finalPassword+getRandomNumber;
+        }
+        if (confirmSpecial){
+          finalPassword=finalPassword+getRandomSpecial;
+        }
+      }
+      else{
+        window.alert ("Please pick at least one criteria for your password. Try again please");
+        generatePassword();
+      }
+
+
+
+
+
+  }
+  
+}
 
 
 
